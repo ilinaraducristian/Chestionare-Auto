ad128f7157ae160ce5b6e894837410fbad1b3b7b
Reydw <oraclle98@gmail.com>
2019-02-20 18:48:08 +0200

    working

--- Chestionare-Auto-Backend/routes/new_session.js
+++ Chestionare-Auto-Backend/routes/new_session.js
@@ -2,57 +2,62 @@ const router = (module.exports = require("express").Router());
 const config = require("../config");
 const jsonwebtoken = require("jsonwebtoken");
 const Session = require("../models/Session");
-const Chestionar = require("../models/Chestionar");
+const mongoose = require("mongoose");
+const question_schema = require("../schemas/question_schema");
 
 router.post("/", (request, response) => {
   verify_input(request.body.category)
-    .then(category => Chestionar(category).new())
+    .then(category => create_new_chestionar(category))
     .then(chestionar => verify_if_chestionar_exists(chestionar))
     .then(chestionar => create_new_session(chestionar))
-    .then(session => session.toObject())
-    .then(session => {
-      session.now = new Date();
-      let res = {
-        session,
-        token: jsonwebtoken.sign(session._id.toString(), config.secret)
-      };
-      delete res.session._id;
-      response.json(res);
-    })
+    .then(session => prepare_response(session.toObject()))
+    .then(res => response.json(res))
     .catch(error => handleError(error, response));
 });
 
 function verify_input(category) {
-  return new Promise((resolve, reject) => {
-    let categories = [
-      "categoria_a",
-      "categoria_b",
-      "categoria_c",
-      "categoria_d"
-    ];
-    if (category === undefined) reject(new Error("category is missing"));
-    else if (categories.findIndex(cat => cat === category) === -1)
-      reject(new Error("wrong category"));
-    else resolve(category);
-  });
+  if (category === undefined)
+    return Promise.reject(new Error("category is missing"));
+  let categories = ["categoria_a", "categoria_b", "categoria_c", "categoria_d"];
+  if (categories.findIndex(cat => cat === category) === -1)
+    return Promise.reject(new Error("wrong category"));
+  return Promise.resolve(category);
+}
+
+function create_new_chestionar(category) {
+  return mongoose
+    .model(category, question_schema, category)
+    .aggregate([{ $sample: { size: 26 } }, { $project: { _id: 0 } }])
+    .exec();
 }
 
 function verify_if_chestionar_exists(chestionar) {
   if (chestionar === null)
     return Promise.reject(new Error("cannot get a new chestionar"));
-  else return Promise.resolve(chestionar);
+  return Promise.resolve(chestionar);
 }
 
 function create_new_session(chestionar) {
-  let now = new Date();
   let new_session = {
-    created_at: now,
-    expiration_date: new Date(now.getTime() + 1800000),
+    created_at: new Date(),
     questions: chestionar,
     correct_answers: 0,
     wrong_answers: 0
   };
   return new Session(new_session).save();
+}
+
+function prepare_response(session) {
+  session.questions.map(question => {
+    delete question.correct_answers;
+    return question;
+  });
+  let response = {
+    session,
+    token: jsonwebtoken.sign(session._id.toString(), config.secret)
+  };
+  delete response.session._id;
+  return response;
 }
 
 function handleError(error, response) {
