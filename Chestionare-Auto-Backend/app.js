b6cc6980a83b6a679f1e87e90ccb7a61f93afbdb
Reydw <oraclle98@gmail.com>
2019-03-22 16:18:08 +0200

    Update

--- Chestionare-Auto-Backend/app.js
+++ Chestionare-Auto-Backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const config = require("./config");
 const cors = require("cors");
+const Session = require("./models/Session");
 
 const db = require("./config").mongoURI;
 
@@ -14,6 +15,12 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+//test
+// let new_session = Session("categoria_a");
+// new_session.then(a => console.log(a));
+
+//test
 
 app.use("/new_session", require("./routes/new_session"));
 app.use("/get_session", require("./routes/get_session"));
--- Chestionare-Auto-Backend/models/Session.js
+++ Chestionare-Auto-Backend/models/Session.js
@@ -1,39 +1,50 @@
 const mongoose = require("mongoose");
 const session_schema = require("../schemas/session");
 const chestionar_schema = require("../schemas/chestionar");
+let Session = mongoose.model("Session", session_schema);
 
-let Session = (module.exports = mongoose.model("Session", session_schema));
-
-function get_chestionare(category) {
-  mongoose
-    .model(category, chestionar_schema, category)
-    .aggregate([{ $sample: { size: 26 } }, { $project: { _id: 0 } }])
-    .exec((err, result) => {
-      return result;
-    });
-}
-
-Session.get_session = function(id) {
-  Session.findById(id).exec((err, res) => {
-    if (err || !res) return Promise.reject("cannot get session");
-    return Promise.resolve(res);
-  });
+module.exports = {
+  new: function(category) {
+    return mongoose
+      .model(category, chestionar_schema, category)
+      .aggregate([{ $sample: { size: 26 } }, { $project: { _id: 0 } }])
+      .exec()
+      .then(chestionare => {
+        if (!chestionare)
+          return Promise.reject(new Error("cannot get chestionare"));
+        return new Session({
+          created_at: new Date(),
+          chestionare,
+          correct_answers: 0,
+          wrong_answers: 0
+        }).save();
+      });
+  },
+  get: function(id) {
+    let return_object = { status: "ok" };
+    Session.findById(id)
+      .exec()
+      .then(session => {
+        if (!session) return Promise.reject("cannot get session");
+        let now = new Date();
+        if (now.getTime() > session.created_at.getTime() + 1800000) {
+          // if time expired
+          if (session.correct_answers > 21) {
+            return_object.ok = "passed";
+          } else {
+            return_object.ok = "failed";
+          }
+        } else {
+          // if not
+          if (session.wrong_answers > 4) {
+            return_object.ok = "failed";
+          } else if (session.correct_answers + session.wrong_answers > 25) {
+            return_object.ok = "passed";
+          } else {
+            return_object.session = session;
+          }
+        }
+        return Promise.resolve(return_object);
+      });
+  }
 };
-
-Session.new_session = function(category) {
-  let chestionare = get_chestionare(category);
-  if (!chestionare) return Promise.reject(new Error("cannot make session"));
-  let new_session = {
-    created_at: new Date(),
-    chestionare,
-    correct_answers: 0,
-    wrong_answers: 0
-  };
-  return new Session(new_session).save();
-};
-
-// nu stiu daca merge
-Session.isExpired = function() {
-  let now = new Date();
-};
-// nu stiu daca merge
